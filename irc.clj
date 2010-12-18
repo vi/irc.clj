dcf314ced701d209a37503b16306ced5420e505f
Vitaly _Vi Shukela <public_vi@tut.by>
2010-12-18 17:39:21 +0200

    Implemented login function and TEST command

--- irc.clj
+++ irc.clj
@@ -1,15 +1,55 @@
 (import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
 (use 'clojure.contrib.server-socket)
+(use 'clojure.contrib.string)
+(defn log [& args] (. java.lang.System/out println (apply str (interpose " " args))))
+
+(defmulti cmd (fn [cmd & args] cmd))
+(defmethod cmd "NICK" [_ & args]
+  (println ":irc.clj 001 * Welcome to _Vi's Clojure IRC \"server\"")
+  (println ":irc.clj 005 * TOPICLEN=65536 PREFIX=(ov)@+ NETWORK=demo CHANTYPES=# : are supported by this demo")
+  (println ":irc.clj 251 * :There are %d users on the server.")
+  (println ":irc.clj 254 * %d :channels formed\n")
+  (println ":irc.clj 375 * MoTH\n")
+)
+(defmethod cmd :default [cmd & args] 
+    (println (format ":irc.clj 421 * %s :Unknown command" cmd))
+)
+(defmethod cmd "TEST" [& args]
+    (doall (map #(println (format ":irc.clj 421 * TEST :Parameter is %s" %)) args))
+)
+
+(defn process-user-input [^String line] 
+ (when-let [result (re-find #"(\w+)(.*)?" line)] 
+  (log "result=" result)
+  (let [command (upper-case (nth result 1))
+	params (trim (nth result 2))]
+    (log "command=" command " params=" params)
+    (let [final-parameter-results (re-find #"(.*):(.*)" params)]
+     (log "final-parameter-results=" final-parameter-results)
+     (if final-parameter-results 
+      (apply cmd command (concat (split #"\s+" (nth final-parameter-results 1)) [(nth final-parameter-results 2)]))
+      (apply cmd command (split #"\s+" params))
+     )
+    )
+  ))
+)
+
 (defn irc-server []
   (letfn [(irc [in out]
                     (binding [*in* (BufferedReader. (InputStreamReader. in))
                               *out* (OutputStreamWriter. out)]
-		      (print "001 Welcome to Clojure demo IRC server")
+		      (println ":irc.clj 439 * :Supply your NICK to procceed")
+		      (flush)
                       (loop []
-                        (let [input (read-line)]
-                          (print input)
-                          (flush))
-                        (recur))))]
+                        (let [line (read-line)]
+			 (when line
+			     (log line)
+			     (process-user-input line)
+			     (flush)
+			     (recur)
+			 ) ; when input
+			) ; read-line
+                      )))]
     (create-server 6667 irc)))
 
 (def my-server (irc-server))
