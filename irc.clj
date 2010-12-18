c61c13b267dc3b6f83f879d876e5c781fee80780
Vitaly _Vi Shukela <public_vi@tut.by>
2010-12-18 18:15:28 +0200

    Keep "user" as state of the connection

--- irc.clj
+++ irc.clj
@@ -3,43 +3,45 @@
 (use 'clojure.contrib.string)
 (defn log [& args] (. java.lang.System/out println (apply str (interpose "|" args))))
 
-(defmulti cmd (fn [cmd & args] cmd))
-(defmethod cmd "NICK" [_ & args]
+(defmulti cmd (fn [^String user cmd & args] cmd))
+(defmethod cmd "NICK" [user _ & args]
   (println ":irc.clj 001 * Welcome to _Vi's Clojure IRC \"server\"")
   (println ":irc.clj 005 * TOPICLEN=65536 PREFIX=(ov)@+ NETWORK=demo CHANTYPES=# : are supported by this demo")
   (println ":irc.clj 251 * :There are %d users on the server.")
   (println ":irc.clj 254 * %d :channels formed")
   (println ":irc.clj 375 * MoTH")
+  user
 )
-(defmethod cmd :default [cmd & args] 
+(defmethod cmd :default [user cmd & args] 
     (println (format ":irc.clj 421 * %s :Unknown command" cmd))
 )
-(defmethod cmd "TEST" [& args]
+(defmethod cmd "TEST" [user & args]
     (doall (map #(println (format ":irc.clj 421 * TEST :Parameter is \"%s\"" %)) args))
 )
-(defmethod cmd "PING" [_ whom & args]
+(defmethod cmd "PING" [user _ whom & args]
     (if (= whom "irc.clj")
     (println ":irc.clj PONG irc.lcj :irc.clj")
     nil ; not implemented
     )
 )
 
-(defn process-user-input [^String line] 
- (when-let [result (re-find #"(\w+)(.*)?" line)] 
-  (let [command (upper-case (nth result 1))
-	params (trim (nth result 2))]
-    (let [final-parameter-results (re-find #"(.*):(.*)" params)]
-     (if final-parameter-results 
-      (if (blank? (nth final-parameter-results 1))
-	(cmd command (nth final-parameter-results 2))
-	(apply cmd (concat [command] (split #"\s+" (nth final-parameter-results 1)) [(nth final-parameter-results 2)])))
-      (if (blank? params)
-	(cmd command)
-	(apply cmd (concat [command] (split #"\s+" params))))
-     )
-    )
-  ))
-)
+(defn process-user-input [user ^String line] 
+ (let [result (re-find #"(\w+)(.*)?" line)] 
+  (if result
+    (let [
+     command (trim (upper-case (nth result 1)))
+     params (trim (nth result 2))
+     newuser (let [final-parameter-results (re-find #"(.*):(.*)" params)]
+	 (if final-parameter-results 
+	  (if (blank? (nth final-parameter-results 1))
+	    (apply cmd [user command (nth final-parameter-results 2)])
+	    (apply cmd (concat [user command] (split #"\s+" (nth final-parameter-results 1)) [(nth final-parameter-results 2)])))
+	  (if (blank? params)
+	    (apply cmd [user command])
+	    (apply  cmd (concat [user command] (split #"\s+" params))))))
+     ]
+     (if (= command "NICK") newuser user))
+    user)))
 
 (defn irc-server []
   (letfn [(irc [in out]
@@ -47,13 +49,13 @@
                               *out* (OutputStreamWriter. out)]
 		      (println ":irc.clj 439 * :Supply your NICK to procceed")
 		      (flush)
-                      (loop []
+                      (loop [user nil]
                         (let [line (read-line)]
 			 (when line
 			     (log line)
-			     (process-user-input line)
-			     (flush)
-			     (recur)
+			     (let [user (process-user-input user line)]
+				 (flush)
+				 (recur user))
 			 ) ; when input
 			) ; read-line
                       )))]
